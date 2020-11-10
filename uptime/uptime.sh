2c683240a86598793994897ee0ed912d7e5b6b0d
Nick Bearman <nick@nickbearman.me.uk>
2020-11-10 16:42:47 +0000

    Updates tweaking email content

--- uptime/uptime.sh
+++ uptime/uptime.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
+echo 'Hello' >> uptime.email
 uptime >> uptime.email
-cat uptime.email | msmtp -a default nick@nickbearman.me.uk;
+cat uptime.email | msmtp -a default nick@nickbearman.me.uk
+rm uptime.email;
 
 
