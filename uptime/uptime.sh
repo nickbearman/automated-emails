b4c358bcc74cbb3ab0766ffb3b62dc6b8c3c6bef
Nick Bearman <nick@geospatialtrainingsolutions.co.uk>
2020-11-11 09:42:07 +0000

    Add in email header

--- uptime/uptime.sh
+++ uptime/uptime.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo 'To:nick@nickbearman.me.uk' >> uptime.email
+echo 'From: pandemic@nickbearman.me.uk' >> uptime.email
+echo 'Subject: Uptime' >> uptime.email
+
 echo 'Hello' >> uptime.email
 uptime >> uptime.email
 cat uptime.email | msmtp -a default nick@nickbearman.me.uk
