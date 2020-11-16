3df8b2600df980a8e98433a54f55f5025b0a9cbc
Nick Bearman <nick@nickbearman.me.uk>
2020-11-16 20:38:32 +0000

    Update layout of uptime email

--- uptime/uptime.sh
+++ uptime/uptime.sh
@@ -4,7 +4,11 @@ echo 'To:nick@nickbearman.me.uk' >> uptime.email
 echo 'From: pandemic@nickbearman.me.uk' >> uptime.email
 echo 'Subject: Uptime' >> uptime.email
 
-echo 'Hello' >> uptime.email
+echo 'Hello Nick' >> uptime.email
+echo 'pandemic has been up since' >> uptime.email
+uptime -s >> uptime.email
+echo 'That is:' >> uptime.email
+uptime -p >> uptime.email
 uptime >> uptime.email
 cat uptime.email | msmtp -a default nick@nickbearman.me.uk
 rm uptime.email;
